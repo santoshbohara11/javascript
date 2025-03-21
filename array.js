8d52661fa7a42421d1e88a9c63d17d7dcf21970c
SantoshBohara <santoshbohara135790@gmail.com>
2025-03-21 21:45:11 +0545

    first commit

---
+++ array.js
@@ -0,0 +1,27 @@
+//array
+// let obj =new obj()
+// const array=['john']
+
+// array.push(10)//;add at last index
+// array.unshift()//add at first index
+
+// arr.pop()  //remove data from the last index
+// array.shift()// remove data from the first index
+const a=[1,2,3,4,5,6]
+const array=['santosh','john','raju']
+
+// console.log(a)
+// console.log(array)
+// console.log(a.length-1)
+// a[0]=20
+console.log(a)
+// a.push(30)
+// a.unshift(11)
+a.pop()
+a.shift()
+console.log(a)
+// console.log(a.length)
+
+
+
+console.log(a[0])
---
+++ object.js
@@ -0,0 +1,16 @@
+// const person ={
+//     name:'santoosh Bohara',
+//     color:'white',
+//     age:23,
+//     job:'developer',
+//     isMarried:true
+// }
+// const obj1=person
+// obj1.name='naresh'
+
+// // person.name='czer'
+// // delete person.name
+// // console.log(person.name)
+// // console.log(person.isMarried)
+// console.log(person)
+// console.log(obj1)
